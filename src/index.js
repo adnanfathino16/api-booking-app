b28eb95267550fb0d455f26f44d396fa770e168e
adnanfathino16 <fathinoadnan@gmail.com>
2023-08-30 17:41:37 +0700

    update access control

--- src/index.js
+++ src/index.js
@@ -16,12 +16,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cookieParser());
 app.use("/uploads", express.static("uploads"));
-app.use(
-  cors({
-    credentials: true,
-    origin: "*",
-  })
-);
+app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
+});
 
 // routes
 routes(app);
